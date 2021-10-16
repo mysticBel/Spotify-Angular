45a3ad4ba14dcd2a784e71bd3c8f83441bfb47f5
mysticBel <dnxbel@gmail.com>
2021-10-16 13:37:13 -0500

    adding route path:hello in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //declaring our first route with an object , property path
+  { 
+    path: '', // refers to localhost:4200/
+    component: ExampleComponent,
+  },
+  {
+    // generating a component 
+    path: 'hello',
+    component: ExampleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

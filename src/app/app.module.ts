5135e8b2845cd487a46387e0e548d6150880b3b1
mysticBel <dnxbel@gmail.com>
2021-10-25 13:17:35 -0500

    deleting ExampleComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExampleComponent } from './example/example.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
   ],
   imports: [
     BrowserModule,

124bd6b316562c96740828b82ec796acc602d78f
mysticBel <dnxbel@gmail.com>
2021-10-25 23:02:53 -0500

    dynamic import:  adding a loadChildren in app-routing.module, importing HomeModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { ExampleComponent } from './example/example.component';
 
 const routes: Routes = [
-  //declaring our first route with an object , property path
   { 
     path: '', // refers to localhost:4200/
-    
+    loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule) //dynamic import
   },
  
 ];
